991d1701652274852ff57371806d220daf17f73b
Anand <anand@anands-mac-pro.local>
2018-08-13 15:25:44 +0530

    Project Completed.

--- CheckLists/AddItemViewController.swift
+++ CheckLists/AddItemViewController.swift
@@ -8,32 +8,59 @@
 
 import UIKit
 
+protocol AddItemViewControllerDelegate: class {
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController)
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
+    func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
+}
+
+// Mark:- Refactor Class name to ItemDetailV
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
-    
     @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    var itemToEdit: ChecklistItem?
+    
+    weak var delegate: AddItemViewControllerDelegate?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //UnComment in iOS 11
+//        UnComment in iOS 11
 //        navigationItem.largeTitleDisplayMode = .never
+        
+        if let item = itemToEdit {
+            title = "Edit Item"
+            textField.text = item.text
+            doneBarButton.isEnabled = true
+        }
+        
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         textField.becomeFirstResponder()
+//        done.isEnabled = false
     }
     
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
+        delegate?.addItemViewControllerDidCancel(self)
     }
     
     @IBAction func done() {
-        navigationController?.popViewController(animated: true)
-        print("Contest of text field: \(textField)")
+        
+        if let itemToEdit = itemToEdit {
+            itemToEdit.text = textField.text!
+            delegate?.addItemViewController(self, didFinishEditing: itemToEdit)
+        } else {
+            let item = ChecklistItem()
+            item.text = textField.text!
+            item.checked = false
+            
+            delegate?.addItemViewController(self, didFinishAdding: item)
+        }
         
     }
     
@@ -44,9 +71,14 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         let oldText = textField.text!
-        let stringRange = Range(range, in:oldText)
-        let newText = oldText.replacingCharacters(in: stringRange!, with: string)
-        if newText.isEmpty {
+//        let stringRange = Range(range, in:oldText)
+        
+//        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        
+//      Mark:- Replace oldtext with newText after uncommenting previous 2 lines in iOS 11
+        
+        if oldText.isEmpty {
             doneBarButton.isEnabled = false
         } else {
             doneBarButton.isEnabled = true
--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController {
+class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
 
+    func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem) {
+        if let index = items.index(of: item) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+                configureText(for: cell, with: item)
+            }
+        }
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem) {
+        
+        let newRowIndex = items.count
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        navigationController?.popViewController(animated: true)
+    }
+    
     var items: [ChecklistItem]
    
     @IBAction func addItem(_ sender: Any) {
@@ -81,6 +107,19 @@ class ChecklistViewController: UITableViewController {
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddItem" {
+            let controller = segue.destination as! AddItemViewController
+            controller.delegate = self
+        } else if segue.identifier == "EditItem" {
+            let controller = segue.destination as! AddItemViewController
+            controller.delegate = self
+            if let indexPath = tableView.indexPath(for: sender as! UITableViewCell) {
+                controller.itemToEdit = items[indexPath.row]
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -131,10 +170,13 @@ class ChecklistViewController: UITableViewController {
     }
 
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        
+        let label = cell.viewWithTag(1001) as! UILabel
+        
         if item.checked {
-            cell.accessoryType = .checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .none
+            label.text = ""
         }
     }
 
